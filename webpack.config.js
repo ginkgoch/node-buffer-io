e758f276bc64efaa4600cc814f710345aed53996
ginkgoch <ginkgoch@outlook.com>
2019-08-03 08:47:07 +0800

    Add webpack.

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+module.exports = {
+  mode: 'production',
+  entry: path.join(__dirname, 'src', 'index.ts'),
+  watch: false,
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: "bundle.js",
+    library: "ginkgoch-buffer-io",
+    libraryTarget: "umd"
+  },
+  module: {
+    rules: [{
+      test: /.tsx?$/,
+      include: [
+        path.resolve(__dirname, 'src')
+      ],
+      exclude: [
+        path.resolve(__dirname, 'node_modules')
+      ],
+      loader: 'ts-loader'
+    }]
+  },
+  resolve: {
+    extensions: ['.json', '.js', '.jsx', '.ts']
+  },
+  devtool: 'source-map'
+};
